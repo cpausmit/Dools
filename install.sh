179ed3c72e609cd16bda9f394ab0807199a0c012
Christoph Paus <paus@mit.edu>
2016-12-20 17:51:51 -0500

    Making sure old bambu versions are out.

--- install.sh
+++ install.sh
@@ -14,10 +14,4 @@ echo "export DOOLS_BASE=`pwd`"                                    >> setup.sh
 echo "export PATH=\"\${PATH}:\${DOOLS_BASE}/bin\""                >> setup.sh
 echo "export PYTHONPATH=\"\${PYTHONPATH}:\${DOOLS_BASE}/python\"" >> setup.sh
 
-# bad fix but needed so curl works withe cmsweb
-echo "source /cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/curl/7.47.1/etc/profile.d/init.sh" \
-                                                                  >> setup.sh
-echo "source /cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/openssl/1.0.2d/etc/profile.d/init.sh" \
-                                                                  >> setup.sh
-
 exit 0
